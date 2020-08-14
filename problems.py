9d335229ba23f478d06177e8b52fda93146be8db
void4 <arndtschnabel@freenet.de>
2020-08-14 14:11:43 +0200

    Add two new challenges

--- problems.py
+++ problems.py
@@ -1,7 +1,46 @@
 # conditions, effects
 problems = [
+
 {
-	"name": "Ships and Factories",
+	"name": "Clickediclick",
+
+	"steps": 25,
+
+	"oneof" : [
+		#["do nothing", [],[]],
+		["manual click", [],[["add", "money",1],]],
+	],
+
+	"always" : [
+	],
+
+	"score": ["get", "money"]
+},
+
+{
+	"name": "Symmetric Ships and Factories",
+
+	"steps": 25,
+
+	"oneof" : [
+		#["do nothing", [],[]],
+		["manual click", [],[["add", "money",1],]],
+		["buy ship", [["atleast", "money",10],], [["add", "money", -10], ["add", "ship", 1]]],
+		["sell ship", [["atleast", "ship",1],],[["add", "money", 10], ["add", "ship", -1]]],
+		["buy factory", [["atleast", "money",20],], [["add", "money", -20], ["add", "factory", 1]]],
+		["sell factory", [["atleast", "factory",1],],[["add", "money", 20], ["add", "factory", -1]]],
+	],
+
+	"always" : [
+		["ship income", [], [["addMultiply", "money", "ship", 3],]],
+		["factory income", [], [["addMultiply", "money", "factory", 5],]],
+	],
+
+	"score": ["get", "money"]
+},
+
+{
+	"name": "Asymmetric Ships and Factories",
 
 	"steps": 25,
 
